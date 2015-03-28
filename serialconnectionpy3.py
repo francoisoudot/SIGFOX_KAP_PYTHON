6d0cdf170f9da5f6f24e661f9cc14206f23d58d2
francois.oudot <francois.oudot@gmail.com>
2015-03-27 19:34:49 -0700

    first commit

---
+++ serialconnectionpy3.py
@@ -0,0 +1,28 @@
+import serial
+import time
+
+# SIGFOX device serial device
+SIGFOX_DEVICE = "/dev/cu.usbserial-AH02DTED"
+# SIGFOX device baud rate
+SIGFOX_DEV_BR = 9600
+# SIGFOX device byte size (5 to 8)
+SIGFOX_DEV_BS = 8
+# SIGFOX device parity ("[N]one", "[E]ven", "[O]dd", "[M]ark", "[S]pace")
+SIGFOX_DEV_PAR = "N"
+# SIGFOX device stop bits (1, 1.5, 2)
+SIGFOX_DEV_SB = 1
+
+
+sigfox = serial.Serial(port=SIGFOX_DEVICE, 
+                       baudrate=SIGFOX_DEV_BR, 
+                       parity=SIGFOX_DEV_PAR,
+                       stopbits=SIGFOX_DEV_SB,
+                       bytesize=SIGFOX_DEV_BS)
+data="at$ss=99999999\r"
+sigfox.write(bytes(data, 'UTF-8'))
+time.sleep(10)
+ans="answer is: "+sigfox.read(sigfox.inWaiting()).decode("utf-8")+" sigfox"
+print(ans)
+
+time.sleep(1)
+sigfox.close()
